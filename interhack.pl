c71e1cad4210eb5813db3f478f082fc52658812b
sartak <sartak@gmail.com>
2007-08-11 00:04:41 -0500

    Simplify telnet negotiation >:)

--- interhack.pl
+++ interhack.pl
@@ -10,35 +10,8 @@ my $socket = new IO::Socket::INET(PeerAddr => 'sporkhack.nineball.org',
 die "Could not create socket: $!\n" unless $socket;
 $socket->blocking(0);
 
-my $IAC = chr(255);
-my $SB = chr(250);
-my $SE = chr(240);
-my $WILL = chr(251);
-my $WONT = chr(252);
-my $DO = chr(253);
-my $DONT = chr(254);
-my $TTYPE = chr(24);
-my $TSPEED = chr(32);
-my $XDISPLOC = chr(35);
-my $NEWENVIRON = chr(39);
-my $IS = chr(0);
-my $GOAHEAD = chr(3);
-my $ECHO = chr(1);
-my $NAWS = chr(31);
-my $STATUS = chr(5);
-my $LFLOW = chr(33);
-
-print {$socket} "$IAC$WILL$TTYPE"
-               ."$IAC$SB$TTYPE${IS}xterm-color$IAC$SE"
-               ."$IAC$WONT$TSPEED"
-               ."$IAC$WONT$XDISPLOC"
-               ."$IAC$WONT$NEWENVIRON"
-               ."$IAC$DONT$GOAHEAD"
-               ."$IAC$WILL$ECHO"
-               ."$IAC$DO$STATUS"
-               ."$IAC$WILL$LFLOW"
-               ."$IAC$WILL$NAWS"
-               ."$IAC$SB$NAWS$IS".chr(80).$IS.chr(24)."$IAC$SE";
+# telnet negotiation...
+print {$socket} "\xFF\xFB\x18\xFF\xFA\x18\x0xterm-color\xFF\xF0\xFF\xFC\x20\xFF\xFC\x23\xFF\xFC\x27\xFF\xFE\x3\xFF\xFB\x1\xFF\xFD\x5\xFF\xFB\x21\xFF\xFB\x1F\xFF\xFA\x1F\x0\x50\x0\x18\xFF\xF0";
 # }}}
 # set up character-based input mode {{{
 ReadMode 3;
