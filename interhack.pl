bf081674b33df3dcf975e80f21d92af24850c2c7
jluehrs2 <jluehrs2@uiuc.edu>
2007-08-13 15:38:55 -0500

    make the code a bit clearer

--- interhack.pl
+++ interhack.pl
@@ -18,6 +18,10 @@ END { ReadMode 0 }
 $| = 1;
 # }}}
 
+sub read_keyboard # {{{
+{
+    ReadKey 0.05;
+} # }}}
 sub read_socket # {{{
 {
     # the reason this is so complicated is because packets can be broken up
@@ -50,19 +54,30 @@ sub read_socket # {{{
         return $from_nao;
     }
 } # }}}
+sub toserver # {{{
+{
+    my $text = shift;
+    print $socket $text;
+} # }}}
+sub toscreen # {{{
+{
+    my $text = shift;
+    print $text;
+} # }}}
 
+# main loop {{{
 while (1)
 {
-    if (defined(my $input = ReadKey 0.05))
+    if (defined(my $input = read_keyboard()))
     {
         $input = "E-  Elbereth\n" if $input eq "\ce"; # ^E writes Elbereth
-        print $socket $input;
+        toserver $input;
     }
 
     if (defined(my $output = read_socket()))
     {
         $output =~ s/Elbereth/\e[35mElbereth\e[m/g; # color E purple
-        print $output;
+        toscreen $output;
     }
-}
+} # }}}
 
