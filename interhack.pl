7b6f1131e37d8033c4a3da2a7896bd8cf288cad3
Shawn M Moore <code@sartak.org>
2013-06-16 15:40:09 -0400

    Get rid of noisy fold comments

--- interhack.pl
+++ interhack.pl
@@ -3,7 +3,7 @@ use IO::Socket::Telnet;
 use Term::ReadKey;
 use Errno 'EAGAIN';
 
-# connect to server {{{
+# connect to server
 my $socket = IO::Socket::Telnet->new(
     PeerAddr => 'nethack.alt.org',
     Proto    => 'tcp',
@@ -13,17 +13,16 @@ $socket->blocking(0);
 
 # telnet negotiation...
 print {$socket} "\xFF\xFB\x18\xFF\xFA\x18\x00xterm-color\xFF\xF0\xFF\xFC\x20\xFF\xFC\x23\xFF\xFC\x27\xFF\xFE\x03\xFF\xFB\x01\xFF\xFD\x05\xFF\xFB\x21\xFF\xFB\x1F\xFF\xFA\x1F\x00\x50\x00\x18\xFF\xF0";
-# }}}
-# set up character-based input mode, autoflush {{{
+
+# set up character-based input mode, autoflush
 ReadMode 3;
 END { ReadMode 0 }
 $| = 1;
-# }}}
 
-sub read_keyboard { # {{{
+sub read_keyboard {
     ReadKey 0.05;
-} # }}}
-sub read_socket { # {{{
+}
+sub read_socket {
     # the reason this is so complicated is because packets can be broken up
     # we can't detect this perfectly, but it's only an issue if an escape code
     # is broken into two parts, and we can check for that
@@ -45,17 +44,20 @@ sub read_socket { # {{{
 
         return $from_server;
     }
-} # }}}
-sub toserver { # {{{
+}
+
+sub toserver {
     my $text = shift;
     print {$socket} $text;
-} # }}}
-sub toscreen { # {{{
+}
+
+sub toscreen {
     my $text = shift;
     print $text;
-} # }}}
+}
 
-# main loop {{{
+
+# main loop
 while (1) {
     if (defined(my $input = read_keyboard)) {
         $input = "E-  Elbereth\n" if $input eq "\ce"; # ^E writes Elbereth
@@ -66,5 +68,5 @@ while (1) {
         $output =~ s/Elbereth/\e[35mElbereth\e[m/g; # color E purple
         toscreen($output);
     }
-} # }}}
+}
 
